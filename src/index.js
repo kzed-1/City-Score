6c89892c22423ca03c1165b5465421561d549651
Kzhao <ken.zh77@gmail.com>
2020-01-06 14:47:09 -0500

    started project skeleton

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-import "/src/styles/index.scss";
+import './styles/index.scss';
+
+let w = 150 
+let h = 175 
+
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .fill("fill", "blue")
